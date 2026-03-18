a87e1b791cee49ac8ae652311a3cc15f02598b75
Nekeroo <77273805+Nekeroo@users.noreply.github.com>
2026-03-18 11:36:16 +0100

    First commit for API

---
+++ cmd/main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"fmt"
+	"os"
+)
+
+func main() {
+
+	cfg := config{
+		addr: ":8080",
+	}
+
+	app := application{
+		config: cfg,
+	}
+
+	if err := app.run(app.mount()); err != nil {
+		fmt.Printf("Server can't start, err : %s", err)
+		os.Exit(1)
+	}
+
+}
